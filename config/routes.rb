8443e5458e10c17e82cef485fead880641349975
Bryan Goss <bcgoss@gmail.com>
2016-10-04 13:21:56 -0600

    Add route and controller for invoice/find

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
+      namespace :invoices do
+        get '/find', to: 'search#find' 
+      end
       resources :merchants, only: [:index, :show]
       resources :invoices, only: [:index, :show]
       resources :items, only: [:index, :show]
