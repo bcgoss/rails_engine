9c372293190204c5a0bed54df2a6600d938dbced
Bryan Goss <bcgoss@gmail.com>
2016-10-04 13:35:31 -0600

    add find for transactions and spec

--- config/routes.rb
+++ config/routes.rb
@@ -2,14 +2,19 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       namespace :invoices do
-        get '/find', to: 'search#find' 
+        get '/find', to: 'search#find'
       end
-      resources :merchants, only: [:index, :show]
       resources :invoices, only: [:index, :show]
+
+      namespace :transactions do
+        get '/find', to: 'search#find'
+      end
+      resources :transactions, only: [:index, :show]
+
+      resources :merchants, only: [:index, :show]
       resources :items, only: [:index, :show]
       resources :invoice_items, only: [:index, :show]
       resources :customers, only: [:index, :show]
-      resources :transactions, only: [:index, :show]
     end
   end
 end
