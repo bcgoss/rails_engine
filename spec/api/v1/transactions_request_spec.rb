bab5cb87d35236e636602b025e3f5f490187927d
Bryan Goss <bcgoss@gmail.com>
2016-10-04 09:44:37 -0600

    Create model routes and controller

---
+++ spec/api/v1/transactions_request_spec.rb
@@ -0,0 +1,34 @@
+require 'rails_helper'
+
+describe 'Transaction CRUD Api' do
+  it 'returns a list of all transactions' do
+    create_list :transaction, 3
+
+    get '/api/v1/transactions'
+
+    transactions = JSON.parse(response.body)
+
+    expect(response.status).to eq(200)
+    expect(transactions.count).to eq(3)
+  end
+
+  it 'returns a single transaction' do
+    transactions = create :transaction, credit_card_number: '2'
+
+    get "/api/v1/transactions/#{transactions.id}"
+
+    raw_transaction = JSON.parse(response.body)
+
+    expect(response.status).to eq(200)
+    expect(raw_transaction['credit_card_number']).to eq('2')
+  end
+
+  it 'finds a transaction based on card number' do
+    get '/api/v1/transactions/find?credit_card_number=2'
+
+    raw_transaction = JSON.parse(response.body)
+
+    expect(response.status).to eq(200)
+    expect(raw_transaction['credit_card_number']).to eq('2')
+  end
+end
---
+++ spec/factories/transactions.rb
@@ -0,0 +1,7 @@
+FactoryGirl.define do
+  factory :transaction do
+    credit_card_number "MyText"
+    credit_card_expiration_date "MyText"
+    result "MyText"
+  end
+end
