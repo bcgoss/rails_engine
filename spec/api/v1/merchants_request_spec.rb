3e19709cb1b92697fd5f6b139fd7bf5b73a72181
Bryan Goss <bcgoss@gmail.com>
2016-10-05 07:02:39 -0600

    Add merchant search actions

--- spec/api/v1/merchants_request_spec.rb
+++ spec/api/v1/merchants_request_spec.rb
@@ -16,4 +16,38 @@ describe 'Merchant CRUD API' do
     expect(response.status).to eq(200)
     expect(raw_merchant["name"]).to eq("Test Merchant")
   end
+
+  it 'finds an merchant by quantity' do
+    create :merchant, name: 'Foo'
+
+    get '/api/v1/merchants/find?name=Foo'
+
+    raw_merchant = JSON.parse(response.body)
+
+    expect(response.status).to eq(200)
+    expect(raw_merchant["name"]).to eq('Foo')
+  end
+
+  it 'finds all merchants by quantity' do
+    create_list :merchant, 2, name: 'Foo'
+    create :merchant
+
+    get '/api/v1/merchants/find_all?name=Foo'
+
+    raw_merchants = JSON.parse(response.body)
+
+    expect(response.status).to eq(200)
+    expect(raw_merchants.count).to eq(2)
+  end
+
+  it 'finds a random merchant' do
+    create_list :merchant, 2
+
+    get '/api/v1/merchants/random.json'
+
+    raw_merchant = JSON.parse(response.body)
+
+    expect(response.status).to eq(200)
+    expect(raw_merchant["name"]).to be_an(String)
+  end
 end
