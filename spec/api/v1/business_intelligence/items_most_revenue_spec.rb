875c3747c87da3c1b3ad24e1ca11873789fb37a7
Bryan Goss <bcgoss@gmail.com>
2016-10-07 08:53:11 -0600

    Add route and model for top items by revenue

--- spec/api/v1/business_intelligence/items_most_revenue_spec.rb
+++ spec/api/v1/business_intelligence/items_most_revenue_spec.rb
@@ -4,14 +4,17 @@ describe "Item, most by revenue" do
   it "returns the top x items" do
     cool_item = create :item, name: "cool toy"
     lame_item = create :item, name: 'lame junk'
-    create_list :invoice, 10, items: [cool_item]
-    create :invoice, items: [lame_item]
+    create :item, name: 'unknown item'
+
+    create_list :complete_invoice, 10, items: [cool_item]
+    create :complete_invoice, items: [lame_item]
 
     get '/api/v1/items/most_revenue?quantity=2'
     items = JSON.parse(response.body)
 
-    expect(reponse.status).to eq(200)
+    expect(response.status).to eq(200)
     expect(items.count).to eq(2)
     expect(items.first["name"]).to eq("cool toy")
+    expect(itmes.last["name"]).to eq('lame junk')
   end
 end
