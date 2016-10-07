9b79758960f918e8737261406170f674d543072a
Bryan Goss <bcgoss@gmail.com>
2016-10-06 19:46:42 -0600

    Partial complet, revenue on date for all merchants

--- app/controllers/api/v1/merchants/revenue_controller.rb
+++ app/controllers/api/v1/merchants/revenue_controller.rb
@@ -1,6 +1,10 @@
 class Api::V1::Merchants::RevenueController < ApplicationController
-  def total
+  def single
     render json: Merchant.find(params[:id]).revenue(params[:date]), serializer: MerchantRevenueSerializer
+  end
+
+  def all
+    render json: Merchant.revenue(params[:date]), serializer: MerchantRevenueSerializer
   end
 
 end
